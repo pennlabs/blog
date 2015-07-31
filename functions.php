1af3c5b3cbedbee2a6d8e64f42571f7dd1de0ac5
Adel Qalieh <aqalieh95@gmail.com>
2015-07-31 19:39:46 -0400

    Add Google Analytics to blog

--- functions.php
+++ functions.php
@@ -181,6 +181,21 @@ if (!function_exists('story_footer_js')) {
 }
 add_action( 'wp_footer', 'story_footer_js', 20, 1 );    
 
+function add_google_analytics() { ?>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-21029575-8', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+<?php
+}
+add_action('wp_footer', 'add_google_analytics');
+
 /**
  * Custom template tags for this theme.
  */
